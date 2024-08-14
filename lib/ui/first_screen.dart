2354713dbff7f7d8a564d5304d549d8df7ab8b4b
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-08-14 14:29:20 +0700

    adding background and customize button

--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -3,6 +3,7 @@ import 'package:palindrome_suitmedia/presentation/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:palindrome_suitmedia/presentation/events.dart';
 import 'package:palindrome_suitmedia/presentation/states.dart';
+import 'package:palindrome_suitmedia/ui/second_screen.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
@@ -14,56 +15,132 @@ class FirstScreen extends StatefulWidget {
 class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: BlocListener<PalindromeBloc, PalindromeState>(
-        listener: (context, state) {
-          if (state is PalindromeCheckedState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.result),
-              ),
-            );
-            context.read<PalindromeBloc>().textController.clear();
-            context.read<PalindromeBloc>().add(ResetStateEvent());
-          }
-        },
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 80,
-                  backgroundImage: AssetImage('assets/images/ic_photo.png'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover),
+        ),
+        child: BlocListener<PalindromeBloc, PalindromeState>(
+          listener: (context, state) {
+            if (state is PalindromeCheckedState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.result),
                 ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: context.read<PalindromeBloc>().textController,
-                  decoration: InputDecoration(
-                    labelText: 'Palindrome',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+              );
+              context.read<PalindromeBloc>().textController.clear();
+              context.read<PalindromeBloc>().add(ResetStateEvent());
+            } else if (state is NavigateToSecondScreenState) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen(
+                    name: state.name,
                   ),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // context.read<PalindromeBloc>().add(CheckPalindromeEvent(
-                    //     context.read<PalindromeBloc>().textController.text));
-                    final text =
-                        context.read<PalindromeBloc>().textController.text;
-                    if (text.isNotEmpty) {
-                      context
-                          .read<PalindromeBloc>()
-                          .add(CheckPalindromeEvent(text));
-                    }
-                  },
-                  child: Text('Check'),
-                ),
-                SizedBox(height: 20),
-              ],
+              );
+            }
+          },
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white10,
+                    radius: 80,
+                    backgroundImage: AssetImage('assets/images/ic_photo.png'),
+                  ),
+                  SizedBox(height: 80),
+                  TextFormField(
+                    controller: context.read<PalindromeBloc>().nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Nama',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: context.read<PalindromeBloc>().textController,
+                    decoration: InputDecoration(
+                      labelText: 'Palindrome',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(43, 99, 123, 1),
+                        elevation: 0,
+                        minimumSize: Size(size * 0.8, 40)),
+                    onPressed: () {
+                      // context.read<PalindromeBloc>().add(CheckPalindromeEvent(
+                      //     context.read<PalindromeBloc>().textController.text));
+                      final text =
+                          context.read<PalindromeBloc>().textController.text;
+                      if (text.isNotEmpty) {
+                        context
+                            .read<PalindromeBloc>()
+                            .add(CheckPalindromeEvent(text));
+                      }
+                    },
+                    child: Text(
+                      'Check',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: size * 0.04,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(43, 99, 123, 1),
+                        elevation: 0,
+                        minimumSize: Size(size * 0.8, 40)),
+                    onPressed: () {
+                      final name =
+                          context.read<PalindromeBloc>().nameController.text;
+                      if (name.isNotEmpty) {
+                        context
+                            .read<PalindromeBloc>()
+                            .add(NavigateToSecondScreenEvent(name));
+                      }
+                    },
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: size * 0.04,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
